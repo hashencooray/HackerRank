d208d20b19f556de61e05105345ad07b6a9b3b88
hashencooray <hashenrashminda@gmail.com>
2021-10-08 11:07:35 +0530

    first commit

---
+++ src/hackerrank/com/Program1.java
@@ -0,0 +1,11 @@
+package hackerrank.com;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int total= a+b;
+        System.out.println("total = "+total);
+    }
+}
